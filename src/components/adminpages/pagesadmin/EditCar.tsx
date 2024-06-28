810e24bb285e192acb73ff31756439660a5ec23d
justintnwj1 <justintnwj1@gmail.com>
2024-06-28 22:41:47 +0700

    third

--- src/components/adminpages/pagesadmin/EditCar.tsx
+++ src/components/adminpages/pagesadmin/EditCar.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useRef, useState} from "react";
+import { ChangeEvent, useRef, useState} from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function EditCar() {
