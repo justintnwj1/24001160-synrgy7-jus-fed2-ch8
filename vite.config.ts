edf44856f438bf379ce7f76cc3e04188014758e7
justintnwj1 <justintnwj1@gmail.com>
2024-06-29 09:59:51 +0700

    success2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: true,
+    outDir: 'build', // Direktori output build
+    assetsDir: '.', // Direktori aset
+    rollupOptions: {
+      // Konfigurasi tambahan Rollup jika diperlukan
+    },
+  },
+  server: {
+    port: 8000, // Port lokal untuk development server
+  },
+});
